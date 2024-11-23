0b40709a992ffa6f239aa5f6d88aeabab08054b3
FunTom <vogeltanz@utb.cz>
2024-11-23 15:36:46 +0100

    Vlastní validační atribut UpperCase

--- UTB.Eshop24.Domain/Entities/Product.cs
+++ UTB.Eshop24.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using UTB.Eshop24.Domain.Validations;
 
 namespace UTB.Eshop24.Domain.Entities
 {
@@ -8,6 +9,7 @@ namespace UTB.Eshop24.Domain.Entities
     {
         [Required]
         [StringLength(50)]
+        [UpperCase]
         public string? Name { get; set; }
         public string? Description { get; set; }
         [Required]
---
+++ UTB.Eshop24.Domain/Validations/UpperCaseAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTB.Eshop24.Domain.Validations
+{
+    public class UpperCaseAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+                return ValidationResult.Success;
+
+            if (value is string text)
+            {
+                if (text.ToUpper() == text)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult($"The {validationContext.MemberName} field does not contain upper cases only.");
+                }
+            }
+
+            throw new NotImplementedException($"The {nameof(UpperCaseAttribute)} cannot be used for the type: {value.GetType()}.");
+        }
+    }
+}
